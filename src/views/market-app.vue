1e5d4961e56ee4ce2dc049f649c725582601fcba
shforeal <rivena14798@gmail.com>
2022-07-29 00:14:39 +0300

    feat: container

--- src/views/market-app.vue
+++ src/views/market-app.vue
@@ -2,7 +2,7 @@
   <header class="main-header main-layout-container">
     <section class="upper-header">
       <!-- <img class="logo-white" src="../assets/CatwikiLogo.svg" alt="" /> -->
-      <div class="inner-container column g-1">
+      <div class="max-width-container column g-1">
         <div class="logo-white"></div>
         <h2 class="upper-text">
           Get to know more about your <br />
@@ -13,22 +13,23 @@
     </section>
     <section class="below-header flex">
       <!-- TODO: change the position absolute to a container warp -->
-      <div class="column g-4">
-        <div class="flex">
-          <div class="breeds-title">
-            66+ Breeds For you <br />
-            to discover
+      <div class="max-width-container">
+        <div class="column g-4">
+          <div class="flex">
+            <div class="breeds-title">
+              66+ Breeds For you <br />
+              to discover
+            </div>
+            <button class="btn btn-more">See More -></button>
           </div>
-          <button class="btn btn-more">See More -></button>
-        </div>
-        <div class="cat-img-conatiner g-3 flex">
-          <img src="../assets/image 1.png" alt="" />
-          <img src="../assets/image 2.png" alt="" />
-          <img src="../assets/image 3.png" alt="" />
+          <div class="cat-img-conatiner g-3 flex">
+            <img src="../assets/image 1.png" alt="" />
+            <img src="../assets/image 2.png" alt="" />
+            <img src="../assets/image 3.png" alt="" />
+          </div>
         </div>
       </div>
     </section>
-    <test />
   </header>
 </template>
 
@@ -40,7 +41,6 @@
   import marketSort from '../cmps/market-sort.vue'
   import chart from '../cmps/chart.vue'
   import MarketFilter from '../cmps/market-filter.vue'
-  import test from '../cmps/test.vue'
 
   export default {
     name: 'MarketApp',
@@ -52,7 +52,6 @@
       marketSort,
       chart,
       MarketFilter,
-      test,
     },
     data() {
       return {}
